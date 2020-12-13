240a172e4d1a6be5dfbc2fc910dcddda93b399bd
ahmed_eid <kingmedo954@gmail.com>
2020-12-13 10:14:37 +0200

    adding the different kind of models that the content can be

--- courses/models.py
+++ courses/models.py
@@ -30,7 +30,12 @@ the Content model can be of different types so we use a generic relation to poin
         4. item (does not exists in the database but build on the other columns)
     **note: learn more about generic relation in Django**
 -----------------------
-
+the ItemBase model is an abstract model that is inherited in the content types (text, file, image, video)
+    - ItemBase :
+        1. owner (user)
+        *. title, created, updated
+    * the files are uploaded to 'files', the images to 'images and the videos store the urls
+----------------------
 """
 
 
@@ -71,8 +76,36 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name="contents", on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in': (
+        'text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
 
 
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    image = models.ImageField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
