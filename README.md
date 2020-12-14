d23995fb6b4b7c7ec03b2f66328c3210edc196db
ahmed_eid <kingmedo954@gmail.com>
2020-12-14 10:23:59 +0200

    creating a custom field (OrderField) to use to sort with respect to other fields (ex. modules with resect to courses)

--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 <ul>
 <li>download postgresql</li>
 </ul>
-
+<hr>
 
 <h2>apps</h2>
 <h3>courses</h3>
 <p></p>
+<hr>
+
+<h3><u>Notes : </u></h3>
+<table>
+<tbody>
+<tr>
+<td><b>Custom field</b></td>
+<td>When you create custom model fields, make them generic. Avoid hardcoding data that depends on a specific model or field. Your field should work in any model.</td>
+</tr>
+</tbody>
+</table>
---
+++ courses/fields.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+"""
+- OrderField is a custom field build on PositiveIntegerField to add two functionality :
+    1. Automatically assign an order value when no specific order is provided
+    2. order objects with respect to other fields
+"""
+
+
+class OrderField(models.PositiveIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        self.for_fields = for_fields
+        super.__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        # automatically assign value
+        if getattr(model_instance, self.attname) is None:
+            try:
+                qs = self.model.objects.all()
+                # order objects with respect to other fields
+                if self.for_fields:
+                    query = {field: getattr(model_instance, field) for field in self.for_fields}
+                    qs = qs.filter(**query)
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
+            except ObjectDoesNotExist:
+                value = 0
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super().pre_save(model_instance, add)
