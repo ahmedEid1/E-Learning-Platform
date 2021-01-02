baabd93bca509a2ea17d148a6a8a7e5826edd69e
ahmed_eid <kingmedo954@gmail.com>
2021-01-02 16:09:21 +0200

    creating a view to create different form for every content type (handling creating and updating content)

--- courses/urls.py
+++ courses/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
     # --handling the course modules____
-    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update')
-
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    # --creating and updating content--
+    path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(),
+         name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<id>/', views.ContentCreateUpdateView.as_view(),
+         name='module_content_update'),
 
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,5 @@
+from django.forms.models import modelform_factory
+from django.apps import apps
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import View, TemplateResponseMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -5,7 +7,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Course
+from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
 
@@ -73,3 +75,46 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             return redirect("manage_course_list")
         return self.render_to_response({'course': self.course, 'formset': formset})
 
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+
+    template_name = "courses/manage/content/form.html"
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'image', 'video', 'file']:
+            return apps.get_model(app_label='courses', model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner', 'order', 'created', 'updated'])
+
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id :
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
+
+        return super().dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form, 'object': self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj, data=request.POST, files=request.FILES)
+
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                Content.objects.create(module=self.module, item=obj)
+
+            return redirect('module_content_list', self.module.id)
+
+        return self.render_to_response({'form': form, 'object': self.obj})
