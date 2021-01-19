a39f3e5fc98856968fa4ac23f4866e0564280fdb
ahmed_eid <kingmedo954@gmail.com>
2021-01-19 17:55:38 +0200

    chat app done (Backend dev. done ^_^)

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,41 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        return self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' % self.id
+        await self.channel_layer.group_add (
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
 
-    def disconnect(self, code):
-        pass
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard (
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
-        self.send(text_data=json.dumps({'message': message}))
-    
+        await self.channel_layer.group_send (
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
+            }
+        )
+
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps(event))
+
